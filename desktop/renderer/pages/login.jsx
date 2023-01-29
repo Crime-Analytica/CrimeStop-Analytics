a0e643775f5d228672a3007b0c53525ec5df712c
GarretTomlin <garrettomlin808@gmail.com>
2023-01-29 16:15:34 -0500

    added login window - desktop

--- desktop/renderer/pages/login.jsx
+++ desktop/renderer/pages/login.jsx
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
-
+import Router from 'next/router'
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
+  const [error, setError] = useState("");
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError("");
+    if (!email) {
+      setError("Email is required");
+      return;
+    }
+    if (!password) {
+      setError("Password is required");
+      return;
+    }
     try {
       const res = await fetch("http://localhost:80/api/sign-in", {
         method: "POST",
@@ -17,18 +28,16 @@ const Login = () => {
       });
       const data = await res.json();
       if (data.token) {
-        useEffect(() => {
-            localStorage.setItem('token', data.token)
-            router.push('/home')
-          }, [data.token])
+        localStorage.setItem('token', data.token)
+        Router.push('/home')
       } else {
         // Show an error message
+        console.log("Invalid Credentials");
       }
     } catch (err) {
       console.error(err);
     }
   };
-
 
   return (
     <div className="flow-root">  
@@ -41,17 +50,15 @@ const Login = () => {
       
         <div
         className="flex flex-col overflow-hidden bg-white rounded-md shadow-lg max md:flex-row md:w-2/4 lg:max-w-screen-xl mt-0 mb-0 ml-0">          <div
-                className="p-4 py-6 text-white bg-[#e01e37] md:w-80 md:flex-shrink-0 md:flex md:flex-col md:items-center md:w-2/1 md:justify-evenly mt-0 mb-0 ml-0">
+                className="p-4 py-6 text-[#fff] bg-[#e01e37] md:w-80 md:flex-shrink-0 md:flex md:flex-col md:items-center md:w-2/1 md:justify-evenly mt-0 mb-0 ml-0">
                
                 <div className="my-3 text-4xl font-bold tracking-wider text-center">
 
-                    <a href="#">CRA</a>
+                    <Link href="#">CRA</Link>
                 </div>
                
                 <p className="mt-6 font-normal text-center text-gray-300 md:mt-0">
-                    With the power of K-WD, you can now focus only on functionaries for your digital products, while
-                    leaving the
-                    UI design on us!
+                "Unlock a new level of community safety with our cutting-edge crime-stopping technology !"
                 </p>
                 <p className="flex flex-col items-center justify-center mt-10 text-center">
                     <span>Don't have an account?</span>
@@ -61,27 +68,33 @@ const Login = () => {
                     Read our <a href="#" className="underline">terms</a> and <a href="#" className="underline">conditions</a>
                 </p>
             </div>
-            <div className="p-5 bg-[#fff] md:flex-1">
+            <div className="p-5 bg-[#FBFBFB] md:flex-1">
                 <div className="p-5 bg-white md:flex-1">
-                    <h3 className="my-4 text-2xl font-semibold text-gray-700">Account Login</h3>
-                    <form action="http://localhost:80/api/sign-in" method="POST" className="flex flex-col space-y-5 z-10 mid:w-2/3" >
+                    <h3 className="my-4 text-2xl font-semibold text-[#334155]">Account Login</h3>
+                    {error && <div className="error texr-2xl text-[red]">{error}</div>}
+
+                    <form onSubmit={handleSubmit} className="flex flex-col space-y-5 z-10 mid:w-2/3" >
                       <div className="flex flex-col space-y-1">
-                        <label htmlFor="email" className="text-sm font-semibold text-gray-500">Email address</label>
+                        <label htmlFor="email" className="text-sm font-semibold text-[#63748E]">Email address</label>
                         <input
                           type="email"
                           id="email"
                           autoFocus
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
                           className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
                         />
                       </div>
                       <div className="flex flex-col space-y-1">
                         <div className="flex items-center justify-between">
-                          <label htmlFor="password" className="text-sm font-semibold text-gray-500">Password</label>
-                          <a href="#" className="text-sm text-red-300 hover:underline focus:text-blue-800">Forgot Password?</a>
+                          <label htmlFor="password" className="text-sm font-semibold text-[#63748E]">Password</label>
+                          <a href="#" className="text-sm text-[#F2A5A8] hover:underline focus:text-[#1E40AE]">Forgot Password?</a>
                         </div>
                         <input
                           type="password"
                           id="password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
                           className="px-4 py-2 transition duration-300 border border-white-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
                         />
                       </div>
@@ -89,6 +102,8 @@ const Login = () => {
                         <input
                           type="checkbox"
                           id="remember"
+                          checked={rememberMe}
+                         onChange={() => setRememberMe(!rememberMe)}
                           className="w-4 h-4 transition duration-300 rounded focus:ring-2 focus:ring-offset-0 focus:outline-none focus:ring-blue-200"
                         />
                         <label htmlFor="remember" className="text-sm font-semibold text-gray-500">Remember me</label>
@@ -96,7 +111,7 @@ const Login = () => {
                       <div>
                         <button
                           type="submit"
-                          className="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-green-500 rounded-md shadow hover:bg-red-600 focus:outline-none focus:ring-red-200 focus:ring-4"
+                          className="w-full px-4 py-2 text-lg font-semibold text-[#fff] transition-colors duration-300 bg-[#e01e37] rounded-md shadow hover:bg-red-600 focus:outline-none focus:ring-red-200 focus:ring-4"
                           id="login-form">
                           Log in
                         </button>
@@ -104,41 +119,36 @@ const Login = () => {
                       <div className="flex flex-col space-y-5">
                         <span className="flex items-center justify-center space-x-2">
                           <span className="h-px bg-gray-400 w-14"></span>
-                          <span className="font-normal text-gray-500">or login with</span>
+                          <span className="font-normal text-[#63748E]">or login with</span>
                           <span className="h-px bg-gray-400 w-14"></span>
                         </span>
                         <div className="flex flex-col space-y-4">
                           <a
                             href="#"
-                            className="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-gray-800 rounded-md group hover:bg-gray-800 focus:outline-none"
+                            className="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-[#1F2A38] rounded-md group group hover:bg-[#1F2A38] focus:outline-none"
                           >
                             <span>
                               <svg
-                                className="w-5 h-5 text-gray-800 fill-current group-hover:text-white"
+                                className="w-5 h-5 text-[#1F2A38] fill-current group-hover:text-[#fff]"
                                 viewBox="0 0 16 16"
                                 version="1.1"
                                 aria-hidden="true"
                               >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-                                ></path>
+                                <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"></path>
                               </svg>
                             </span>
-                            <span className="text-sm font-medium text-gray-800 group-hover:text-white">Github</span>
+                            <span className="text-sm font-medium text-[#1F2A38] group-hover:text-[#fff]">Google</span>
                           </a>
                           <a
                             href="#"
-                            className="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-gray-800 rounded-md group hover:bg-gray-800 focus:outline-none"
+                            className="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-[#1F2A38] rounded-md group hover:bg-[#1F2A38] focus:outline-none"
                           >
                             <span>
-                              <svg className="text-gray-800 group-hover:text-white" width="20" height="20" fill="currentColor">
-                                <path
-                                  d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"
-                                ></path>
+                              <svg className="text-[#1F2A38] group-hover:text-[#fff]" width="20" height="20" fill="currentColor">
+                              <path d="M7.462 0H0v7.19h7.462V0zM16 0H8.538v7.19H16V0zM7.462 8.211H0V16h7.462V8.211zm8.538 0H8.538V16H16V8.211z"/>
                               </svg>
                             </span>
-                            <span className="text-sm font-medium text-grey-800 group-hover:text-white">Twitter</span>
+                            <span className="text-sm font-medium  text-[#1F2A38] group-hover:text-[#fff]">Microsoft</span>
                           </a>
                         </div>
                       </div>
