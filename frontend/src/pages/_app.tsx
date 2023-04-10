d8614cd5efd7959435a9ff928dad7fc86472bf96
GarretTomlin <garrettomlin808@gmail.com>
2023-04-10 12:20:04 -0500

    updated meta tags

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <meta name="keywords" content="CrimeStop-Analytics, Crime Analysis, CrimeStop, Jamaica" />
     <meta name="author" content="Garret Tomlin" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="og:image" content="https://res.cloudinary.com/dwhs4luwi/image/upload/v1681147022/icon_tsvzxl.png"/>
   </Head></>
   
   )
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -16,6 +16,7 @@ function Home() {
         <meta name="keywords" content="CrimeStop-Analytics, Crime Analysis, CrimeStop, Jamaica" />
         <meta name="author" content="Garret Tomlin" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="og:image" content="https://res.cloudinary.com/dwhs4luwi/image/upload/v1681147022/icon_tsvzxl.png"/>
       </Head>
       <NavBar />
       <div className="relative flex flex-col md:flex-row bg-[#1e1e1e] top-0">
